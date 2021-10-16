0e4afdc7339111d3948553a2d9b74334ae523b96
Julia Trefas <JTR@Julias-MacBook-Pro.local>
2021-10-16 14:32:09 +0200

    Create profiles component and do some initial styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import './profiles/Index'
+import Profiles from './profiles/Index';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Profiles/>
       </header>
     </div>
   );
---
+++ src/profiles/Index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Index.css';
+
+function Profiles() {
+  return (
+    <div className="profiles">
+        <h1 className="heading-1">Profiles</h1>
+    </div>
+  );
+}
+
+export default Profiles;
